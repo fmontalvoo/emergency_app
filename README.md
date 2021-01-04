72d12a58f499c70dc4c69ca8eb39923014ab497c
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-01-04 14:53:23 -0500

    First commit.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Diseño de aplicación con Flutter
+
+<p align="center">
+    <img src="img.png" alt="App">
+</p>
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:emergency_app/src/pages/emergency_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Emergency App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: EmergencyPage(),
+    );
+  }
+}
